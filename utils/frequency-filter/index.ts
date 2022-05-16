fc881334ba71bf9b711769f720613fd9e0e7347d
phamquangsang661 <phamquangsang661@gmail.com>
2022-05-16 19:00:53 +0000

    fix:delete test in index

--- utils/frequency-filter/index.ts
+++ utils/frequency-filter/index.ts
@@ -4,4 +4,3 @@ export * from './butterworth-low-pass-filter';
 export * from './butterworth-high-pass-filter';
 export * from './gaussian-low-pass-filter';
 export * from './gaussian-high-pass-filter';
-export * from './test';
